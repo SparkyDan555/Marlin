a2ec1aad34d608a5fa6f9c682725e8ff4f780ecd
SparkyDan555 <dwhite56@hotmail.co.uk>
2020-07-26 12:25:45 +0100

    Added Preheat PETG Constant

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1513,6 +1513,11 @@
 #define PREHEAT_2_TEMP_BED    100
 #define PREHEAT_2_FAN_SPEED     0 // Value from 0 to 255
 
+#define PREHEAT_3_LABEL "PETG"
+#define PREHEAT_3_TEMP_HOTEND 230
+#define PREHEAT_3_TEMP_BED 80
+#define PREHEAT_3_FAN_SPEED 0 // Value from 0 to 255
+
 /**
  * Nozzle Park
  *
