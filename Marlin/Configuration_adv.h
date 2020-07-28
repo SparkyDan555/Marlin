8176bbb7022d10f93000053e99ce5cfcc7de40df
SparkyDan555 <dwhite56@hotmail.co.uk>
2020-07-28 23:42:25 +0100

    Improve Buffers
    Faster response

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1841,14 +1841,14 @@
 #elif ENABLED(SDSUPPORT)
   #define BLOCK_BUFFER_SIZE 32
 #else
-  #define BLOCK_BUFFER_SIZE 16
+  #define BLOCK_BUFFER_SIZE 64
 #endif
 
 // @section serial
 
 // The ASCII buffer for serial input
 #define MAX_CMD_SIZE 96
-#define BUFSIZE 4
+#define BUFSIZE 64
 
 // Transmission to Host Buffer Size
 // To save 386 bytes of PROGMEM (and TX_BUFFER_SIZE+3 bytes of RAM) set to 0.
