0914004d78d7cf544fe1c1e8566936e82d0671ba
SparkyDan555 <38540799+SparkyDan555@users.noreply.github.com>
2022-07-22 14:31:01 +0100

    Fix Configuration Issues

--- Marlin/src/pins/lpc1768/pins_BTT_SKR_V1_4.h
+++ Marlin/src/pins/lpc1768/pins_BTT_SKR_V1_4.h
@@ -315,9 +315,9 @@
     #define BTN_EN2                  EXP1_07_PIN
     #define BTN_ENC                  EXP1_01_PIN
 
-    #define LCD_PINS_ENABLE          EXP1_08_PIN
-    #define LCD_PINS_D4              EXP1_06_PIN
-    #define BEEPER_PIN               EXP1_09_PIN
+    #define LCD_PINS_ENABLE          EXP1_03_PIN
+    #define LCD_PINS_D4              EXP1_05_PIN
+    #define BEEPER_PIN               EXP1_02_PIN
 
   #elif ENABLED(ANET_FULL_GRAPHICS_LCD)
     #ifndef NO_CONTROLLER_CUSTOM_WIRING_WARNING
