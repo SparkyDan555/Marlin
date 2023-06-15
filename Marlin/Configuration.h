68a0222ece2eb78d7c68e2b62e37d22c3acb03af
SparkyDan555 <38540799+SparkyDan555@users.noreply.github.com>
2023-06-15 13:51:53 +0100

    Tone Queue

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1840,7 +1840,7 @@
   #define FIL_RUNOUT_PULLUP               // Use internal pullup for filament runout pins.
   //#define FIL_RUNOUT_PULLDOWN           // Use internal pulldown for filament runout pins.
   //#define WATCH_ALL_RUNOUT_SENSORS      // Execute runout script on any triggering sensor, not only for the active extruder.
-                                          // This is automatically enabled for MIXING_EXTRUDERs. (Commented out in Conditionals_adv.h for Printer 1 Build)
+                                          // This is automatically enabled for MIXING_EXTRUDERs.
 
   // Override individually if the runout sensors vary
   //#define FIL_RUNOUT1_STATE LOW
@@ -2699,7 +2699,7 @@
 // Tone queue size, used to keep beeps from blocking execution.
 // Default is 4, or override here. Costs 4 bytes of SRAM per entry.
 //
-//#define TONE_QUEUE_LENGTH 4
+#define TONE_QUEUE_LENGTH 10
 
 //
 // A sequence of tones to play at startup, in pairs of tone (Hz), duration (ms).
