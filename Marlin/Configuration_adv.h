209356c4736c9fe7662d8e75567f57642ae248b9
SparkyDan555 <38540799+SparkyDan555@users.noreply.github.com>
2024-09-07 14:30:07 +0100

    Fix jittering on small circles

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1270,7 +1270,7 @@
 // Increase the slowdown divisor for larger buffer sizes.
 #define SLOWDOWN
 #if ENABLED(SLOWDOWN)
-  #define SLOWDOWN_DIVISOR 2
+  #define SLOWDOWN_DIVISOR 4
 #endif
 
 /**
@@ -2619,7 +2619,7 @@
 #if ALL(HAS_MEDIA, DIRECT_STEPPING)
   #define BLOCK_BUFFER_SIZE  8
 #elif HAS_MEDIA
-  #define BLOCK_BUFFER_SIZE 16
+  #define BLOCK_BUFFER_SIZE 64
 #else
   #define BLOCK_BUFFER_SIZE 16
 #endif
